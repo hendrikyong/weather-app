36400a2535e65a031c52d167d12046a8e472a546
Hendrik Yong <hendrikyong1205@gmail.com>
2024-10-09 20:54:15 +0800

    header completed

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM loaded");
+
+  const search = document.getElementById("search");
+  search.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      //logs what has been input into search box
+      console.log(search.value);
+      //resets the search box to make it empty
+      search.value = "";
+    }
+  });
 });
